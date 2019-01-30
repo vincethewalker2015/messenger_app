d6ec09338f853650f639732e437a956108d8464b
vincentbwalker <vincethewalker@gmail.com>
2019-01-30 11:26:28 +0000

    removed i18

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.i18n.fallbacks = true
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
